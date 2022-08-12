f0c7e91653b770856446b3e102570b48f54d23e4
Akhalil123321 <ahmed.mohamed.khalil1998@gmail.com>
2022-08-12 19:38:18 +0200

    Add My Project Route

---
+++ src/data/data.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+    {
+        projectName:'TEk Store',
+        porjectPicture:'tek.png',
+        projectURL:'https://tek-store.netlify.app/'
+    },
+    {
+        projectName:'Hotel-Room-Booking',
+        porjectPicture:'room boking.png',
+        projectURL:'https://hotel-room-booking2.netlify.app/'
+    },
+    {
+        projectName:'Meme Generator',
+        porjectPicture:'meme generator.png',
+        projectURL:'https://meme-generator75.netlify.app/'
+    },
+    
+]
--- src/routes/MyProjects.jsx
+++ src/routes/MyProjects.jsx
@@ -1,12 +1,29 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
 import {Link} from "react-router-dom";
-export default function App(){
-
+import DencryptEffect from '../components/DencryptEffect'
+import Data from '../data/data'
+export default function MyProjects(){
+const project = Data.map(items => {
+    return(
+        <section className='card-one'>
+                <a href={items.projectURL}target="_blank" className='image-cont'>
+                    <img src={`./images/projects/${items.porjectPicture}`} alt="" className='project-image' />
+                </a>
+                <h1 className='project-name'><DencryptEffect text={items.projectName}/></h1>
+        </section>
+    )
+})
 return(
-    <div>
-        <div className='dark-all-cont'>
-            <img src='./images/watermark3.png' className='water-mark' alt=""/>
-            <Link to='/'><img src='./images/png 3.png' className="logo" alt=""/></Link>
+    <div className='my-projects-cont'>
+        {/* <img src='./images/watermark3.png' className='water-mark' alt=""/> */}
+        <div>
+            <Link to='/'>
+            <img src='./images/png 3.png' className="my-projects-logo" alt=""/>
+            </Link>
+        </div>
+        <div className='card-cont'>
+            {project}
         </div>
     </div>
 )}
