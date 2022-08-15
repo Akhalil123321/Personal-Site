444c70691993b11270861ec2af57ab8bd91827d7
Ahmed khalil <ahmed.mohamed.khalil1998@gmail.com>
2022-08-14 20:48:09 -0700

    upgrede projects

--- src/data/data.js
+++ src/data/data.js
@@ -6,7 +6,7 @@ export default [
         projectURL:'https://tek-store.netlify.app/'
     },
     {
-        projectName:'Hotel-Room-Booking',
+        projectName:'Hotel Room Booking',
         porjectPicture:'room boking.png',
         projectURL:'https://hotel-room-booking2.netlify.app/'
     },
@@ -14,6 +14,21 @@ export default [
         projectName:'Meme Generator',
         porjectPicture:'meme generator.png',
         projectURL:'https://meme-generator75.netlify.app/'
+    },
+    {
+        projectName:'Gym Website',
+        porjectPicture:'gym-site.png',
+        projectURL:'https://gymwibsite.netlify.app/'
+    },
+    {
+        projectName:'Password Generator Extension',
+        porjectPicture:'password generetor.png',
+        projectURL:'https://github.com/Akhalil123321/Password-Generator-Chrome-Extension'
+    },
+    {
+        projectName:'URL Depository Extintion',
+        porjectPicture:'url extention.png',
+        projectURL:'https://github.com/Akhalil123321/URL-Depository-Chrome-Extintion'
     },
     
 ]
